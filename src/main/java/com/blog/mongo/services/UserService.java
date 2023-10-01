dd7b91142858df16253bd8bbbe6be8fea95b13cf
Jbfaneto <jbfaneto@gmail.com>
2023-10-01 19:04:49 -0300

    New feat: findById added and custom exceptions

--- src/main/java/com/blog/mongo/services/UserService.java
+++ src/main/java/com/blog/mongo/services/UserService.java
@@ -3,10 +3,12 @@ package com.blog.mongo.services;
 import com.blog.mongo.domain.User;
 
 import com.blog.mongo.repository.UserRepository;
+import com.blog.mongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,4 +19,8 @@ public class UserService {
         return repository.findAll();
     }
 
+    public User findById(String id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
 }
