8a161fad0fa250fd571609d336e971507fa6ac50
Jbfaneto <jbfaneto@gmail.com>
2023-10-01 11:45:17 -0300

    UserService, UserRepository created and working with mongoDB

---
+++ src/main/java/com/blog/mongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.blog.mongo.repository;
+
+import com.blog.mongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
---
+++ src/main/java/com/blog/mongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.blog.mongo.services;
+
+import com.blog.mongo.domain.User;
+
+import com.blog.mongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+}
