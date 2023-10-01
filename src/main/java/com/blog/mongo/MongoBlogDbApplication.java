8efe5e394c89f6711341071b102e0f22c4e3c9fb
Jbfaneto <jbfaneto@gmail.com>
2023-10-01 09:14:28 -0300

    First commit beginning of the project

---
+++ src/main/java/com/blog/mongo/MongoBlogDbApplication.java
@@ -0,0 +1,13 @@
+package com.blog.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoBlogDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoBlogDbApplication.class, args);
+	}
+
+}
