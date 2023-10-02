680197af0093f38b8114a4ea9f1839dedc524874
Jbfaneto <jbfaneto@gmail.com>
2023-10-01 21:46:07 -0300

    Created Post entity with nested authors

---
+++ src/main/java/com/blog/mongo/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.blog.mongo.repository;
+
+import com.blog.mongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
